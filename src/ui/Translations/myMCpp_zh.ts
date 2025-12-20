168d2e4504ddd5bb3bc7d6384445449a7949ce5b
Stern <xsternent@gmail.com>
2025-12-20 00:50:01 -0500

    New translations mymcpp_en.ts (Chinese Traditional) [ci skip]

--- src/ui/Translations/myMCpp_zh.ts
+++ src/ui/Translations/myMCpp_zh.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh-CN" sourcelanguage="en-US">
+<TS version="2.1" language="zh-TW" sourcelanguage="en-US">
   <context>
     <name>MainWindow</name>
     <message>
