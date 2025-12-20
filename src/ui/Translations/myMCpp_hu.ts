1c68d330c1d8cec67adfbc5af5343ab7bf61724f
Stern <xsternent@gmail.com>
2025-12-20 00:49:49 -0500

    New translations mymcpp_en.ts (Hungarian) [ci skip]

---
+++ src/ui/Translations/myMCpp_hu.ts
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu" sourcelanguage="en-US">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <location filename="../MainWindow.cpp" line="+81"/>
+      <source>Imported %1</source>
+      <translation type="unfinished">Imported %1</translation>
+    </message>
+    <message>
+      <location line="+27"/>
+      <source>Open Memory Card</source>
+      <translation type="unfinished">Open Memory Card</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>PS2 Memory Card (*.ps2 *.mc2 *.mcd *.bin *.mc);;All Files (*.*)</source>
+      <translation type="unfinished">PS2 Memory Card (*.ps2 *.mc2 *.mcd *.bin *.mc);;All Files (*.*)</translation>
+    </message>
+    <message>
+      <location line="+40"/>
+      <source>Create Memory Card</source>
+      <translation type="unfinished">Create Memory Card</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <location line="+274"/>
+      <source>PCSX2 Memory Card (*.ps2);;MemCard PRO2 (*.mc2 *.mcd);;All Files (*.*)</source>
+      <translation type="unfinished">PCSX2 Memory Card (*.ps2);;MemCard PRO2 (*.mc2 *.mcd);;All Files (*.*)</translation>
+    </message>
+    <message>
+      <location line="-249"/>
+      <source>Created %1 MB memory card</source>
+      <translation type="unfinished">Created %1 MB memory card</translation>
+    </message>
+    <message>
+      <location line="+18"/>
+      <source>Import Save</source>
+      <translation type="unfinished">Import Save</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>PS2 Save Files (*.psu *.max *.sps *.xps *.cbs *.psv);;EMS/PSU (*.psu);;MAX Drive (*.max);;SharkPort (*.sps);;X-Port (*.xps);;CodeBreaker (*.cbs);;PSV (*.psv);;All Files (*.*)</source>
+      <translation type="unfinished">PS2 Save Files (*.psu *.max *.sps *.xps *.cbs *.psv);;EMS/PSU (*.psu);;MAX Drive (*.max);;SharkPort (*.sps);;X-Port (*.xps);;CodeBreaker (*.cbs);;PSV (*.psv);;All Files (*.*)</translation>
+    </message>
+    <message>
+      <location line="+15"/>
+      <source>Export</source>
+      <translation type="unfinished">Export</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Please select a save to export</source>
+      <translation type="unfinished">Please select a save to export</translation>
+    </message>
+    <message>
+      <location line="+6"/>
+      <source>Export Save</source>
+      <translation type="unfinished">Export Save</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>EMS/PSU Format (*.psu);;MAX Drive Format (*.max);;All Files (*.*)</source>
+      <translation type="unfinished">EMS/PSU Format (*.psu);;MAX Drive Format (*.max);;All Files (*.*)</translation>
+    </message>
+    <message>
+      <location line="+15"/>
+      <source>Delete</source>
+      <translation type="unfinished">Delete</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Please select a save to delete</source>
+      <translation type="unfinished">Please select a save to delete</translation>
+    </message>
+    <message>
+      <location line="+9"/>
+      <source>Delete Save</source>
+      <translation type="unfinished">Delete Save</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Are you sure you want to delete &apos;%1&apos;?</source>
+      <translation type="unfinished">Are you sure you want to delete &apos;%1&apos;?</translation>
+    </message>
+    <message>
+      <location line="+8"/>
+      <source>Deleted %1</source>
+      <translation type="unfinished">Deleted %1</translation>
+    </message>
+    <message>
+      <location line="+36"/>
+      <source>About myMCpp</source>
+      <translation type="unfinished">About myMCpp</translation>
+    </message>
+    <message>
+      <location line="+34"/>
+      <source>Check for Updates</source>
+      <translation type="unfinished">Check for Updates</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>You are running the latest version of myMCpp (v1.0.0).
+
+For updates, visit: https://github.com/SternXD/myMCpp/releases</source>
+      <translation type="unfinished">You are running the latest version of myMCpp (v1.0.0).
+
+For updates, visit: https://github.com/SternXD/myMCpp/releases</translation>
+    </message>
+    <message>
+      <location line="+57"/>
+      <source>Free space: %1 MB</source>
+      <translation type="unfinished">Free space: %1 MB</translation>
+    </message>
+    <message>
+      <location line="+4"/>
+      <source>Memory card open</source>
+      <translation type="unfinished">Memory card open</translation>
+    </message>
+    <message>
+      <location line="+5"/>
+      <source>No memory card open</source>
+      <translation type="unfinished">No memory card open</translation>
+    </message>
+    <message>
+      <location line="+32"/>
+      <source>Save Memory Card As</source>
+      <translation type="unfinished">Save Memory Card As</translation>
+    </message>
+    <message>
+      <location line="+10"/>
+      <location line="+44"/>
+      <source>Saved to %1</source>
+      <translation type="unfinished">Saved to %1</translation>
+    </message>
+    <message>
+      <location line="-40"/>
+      <location line="+44"/>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <location line="-43"/>
+      <location line="+44"/>
+      <source>Failed to save: %1</source>
+      <translation type="unfinished">Failed to save: %1</translation>
+    </message>
+    <message>
+      <location line="-19"/>
+      <source>Remove ECC and Save As...</source>
+      <translation type="unfinished">Remove ECC and Save As...</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Add ECC and Save As...</source>
+      <translation type="unfinished">Add ECC and Save As...</translation>
+    </message>
+    <message>
+      <location line="+6"/>
+      <source>PS2 Memory Card (*.ps2 *.mc2 *.mcd);;All Files (*.*)</source>
+      <translation type="unfinished">PS2 Memory Card (*.ps2 *.mc2 *.mcd);;All Files (*.*)</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsDialog</name>
+    <message>
+      <location filename="../dialogs/SettingsDialog.cpp" line="+144"/>
+      <source>Select Memory Card Folder</source>
+      <translation type="unfinished">Select Memory Card Folder</translation>
+    </message>
+  </context>
+</TS>
