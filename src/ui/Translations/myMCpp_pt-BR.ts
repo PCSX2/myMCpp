4b6aeefc81e41ce0d37afab1ed189baf12b52a02
Stern <xsternent@gmail.com>
2025-12-21 01:42:48 -0500

    New translations mymcpp_en.ts (Portuguese, Brazilian) [ci skip]

--- src/ui/Translations/myMCpp_pt-BR.ts
+++ src/ui/Translations/myMCpp_pt-BR.ts
@@ -6,160 +6,161 @@
     <message>
       <location filename="../MainWindow.cpp" line="+81"/>
       <source>Imported %1</source>
-      <translation type="unfinished">Imported %1</translation>
+      <translation>Importado %1</translation>
     </message>
     <message>
       <location line="+27"/>
       <source>Open Memory Card</source>
-      <translation type="unfinished">Open Memory Card</translation>
+      <translation>Abrir Cartão de Memória</translation>
     </message>
     <message>
       <location line="+2"/>
       <source>PS2 Memory Card (*.ps2 *.mc2 *.mcd *.bin *.mc);;All Files (*.*)</source>
-      <translation type="unfinished">PS2 Memory Card (*.ps2 *.mc2 *.mcd *.bin *.mc);;All Files (*.*)</translation>
+      <translation>Cartão de Memória PS2 (*.ps2 *.mc2 *.mcd *.bin *.mc);;Todos os Arquivos (*.*)</translation>
     </message>
     <message>
       <location line="+40"/>
       <source>Create Memory Card</source>
-      <translation type="unfinished">Create Memory Card</translation>
+      <translation>Criar Cartão de Memória</translation>
     </message>
     <message>
       <location line="+2"/>
       <location line="+274"/>
       <source>PCSX2 Memory Card (*.ps2);;MemCard PRO2 (*.mc2 *.mcd);;All Files (*.*)</source>
-      <translation type="unfinished">PCSX2 Memory Card (*.ps2);;MemCard PRO2 (*.mc2 *.mcd);;All Files (*.*)</translation>
+      <translation>Cartão de Memória PCSX2 (*.ps2);;MemCard PRO2(*.mc2 *.mcd);;Todos os Arquivos (*.*)</translation>
     </message>
     <message>
       <location line="-249"/>
       <source>Created %1 MB memory card</source>
-      <translation type="unfinished">Created %1 MB memory card</translation>
+      <translation>Criou Cartão de Memória de %1 MB</translation>
     </message>
     <message>
       <location line="+18"/>
       <source>Import Save</source>
-      <translation type="unfinished">Import Save</translation>
+      <translation>Importar jogo salvo</translation>
     </message>
     <message>
       <location line="+2"/>
       <source>PS2 Save Files (*.psu *.max *.sps *.xps *.cbs *.psv);;EMS/PSU (*.psu);;MAX Drive (*.max);;SharkPort (*.sps);;X-Port (*.xps);;CodeBreaker (*.cbs);;PSV (*.psv);;All Files (*.*)</source>
-      <translation type="unfinished">PS2 Save Files (*.psu *.max *.sps *.xps *.cbs *.psv);;EMS/PSU (*.psu);;MAX Drive (*.max);;SharkPort (*.sps);;X-Port (*.xps);;CodeBreaker (*.cbs);;PSV (*.psv);;All Files (*.*)</translation>
+      <translation>Arquivo de jogo salvo PS2 (*.psu *.max *.sps *.xps *.cbs *.psv);;EMS/PSU (*.psu);;MAX Drive (*.max);;SharkPort (*.sps);;X-Port (*.xps);;CodeBreaker (*.cbs);;PSV (*.psv);;Todos os Arquivos (*.*)</translation>
     </message>
     <message>
       <location line="+15"/>
       <source>Export</source>
-      <translation type="unfinished">Export</translation>
+      <translation>Exportar</translation>
     </message>
     <message>
       <location line="+1"/>
       <source>Please select a save to export</source>
-      <translation type="unfinished">Please select a save to export</translation>
+      <translation>Por favor, selecione um jogo salvo para exportar</translation>
     </message>
     <message>
       <location line="+6"/>
       <source>Export Save</source>
-      <translation type="unfinished">Export Save</translation>
+      <translation>Exportar Jogo Salvo</translation>
     </message>
     <message>
       <location line="+2"/>
       <source>EMS/PSU Format (*.psu);;MAX Drive Format (*.max);;All Files (*.*)</source>
-      <translation type="unfinished">EMS/PSU Format (*.psu);;MAX Drive Format (*.max);;All Files (*.*)</translation>
+      <translation>Formato EMS/PSU (*.psu);;Formato MAX Drive (*.max);;Todos os Arquivos (*.*)</translation>
     </message>
     <message>
       <location line="+15"/>
       <source>Delete</source>
-      <translation type="unfinished">Delete</translation>
+      <translation>Excluir</translation>
     </message>
     <message>
       <location line="+1"/>
       <source>Please select a save to delete</source>
-      <translation type="unfinished">Please select a save to delete</translation>
+      <translation>Por favor, escolha um jogo salvo para excluir</translation>
     </message>
     <message>
       <location line="+9"/>
       <source>Delete Save</source>
-      <translation type="unfinished">Delete Save</translation>
+      <translation>Excluir jogo salvo</translation>
     </message>
     <message>
       <location line="+1"/>
       <source>Are you sure you want to delete &apos;%1&apos;?</source>
-      <translation type="unfinished">Are you sure you want to delete &apos;%1&apos;?</translation>
+      <translation>Você tem certeza que quer excluir &apos;%1&apos;?</translation>
     </message>
     <message>
       <location line="+8"/>
       <source>Deleted %1</source>
-      <translation type="unfinished">Deleted %1</translation>
+      <translation>Excluiu %1</translation>
     </message>
     <message>
       <location line="+36"/>
       <source>About myMCpp</source>
-      <translation type="unfinished">About myMCpp</translation>
+      <translation>Sobre myMCpp</translation>
     </message>
     <message>
       <location line="+34"/>
       <source>Check for Updates</source>
-      <translation type="unfinished">Check for Updates</translation>
+      <translation>Procurar Atualizações</translation>
     </message>
     <message>
       <location line="+1"/>
       <source>You are running the latest version of myMCpp (v1.0.0).
 
 For updates, visit: https://github.com/SternXD/myMCpp/releases</source>
-      <translation type="unfinished">You are running the latest version of myMCpp (v1.0.0).
+      <translation>Você está utilizando a versão mais recente do myMCpp (v1.0.0).
 
-For updates, visit: https://github.com/SternXD/myMCpp/releases</translation>
+Para atualizações, visite:
+https://github.com/SternXD/myMCpp/releases</translation>
     </message>
     <message>
       <location line="+57"/>
       <source>Free space: %1 MB</source>
-      <translation type="unfinished">Free space: %1 MB</translation>
+      <translation>Espaço livre: %1 MB</translation>
     </message>
     <message>
       <location line="+4"/>
       <source>Memory card open</source>
-      <translation type="unfinished">Memory card open</translation>
+      <translation>Cartão de Memória aberto</translation>
     </message>
     <message>
       <location line="+5"/>
       <source>No memory card open</source>
-      <translation type="unfinished">No memory card open</translation>
+      <translation>Nenhum Cartão de Memória aberto</translation>
     </message>
     <message>
       <location line="+32"/>
       <source>Save Memory Card As</source>
-      <translation type="unfinished">Save Memory Card As</translation>
+      <translation>Salvar Cartão de Memória Como</translation>
     </message>
     <message>
       <location line="+10"/>
       <location line="+44"/>
       <source>Saved to %1</source>
-      <translation type="unfinished">Saved to %1</translation>
+      <translation>Salvo para %1</translation>
     </message>
     <message>
       <location line="-40"/>
       <location line="+44"/>
       <source>Error</source>
-      <translation type="unfinished">Error</translation>
+      <translation>Erro</translation>
     </message>
     <message>
       <location line="-43"/>
       <location line="+44"/>
       <source>Failed to save: %1</source>
-      <translation type="unfinished">Failed to save: %1</translation>
+      <translation>Erro ao salvar: %1</translation>
     </message>
     <message>
       <location line="-19"/>
       <source>Remove ECC and Save As...</source>
-      <translation type="unfinished">Remove ECC and Save As...</translation>
+      <translation>Remover ECC e Salvar Como...</translation>
     </message>
     <message>
       <location line="+0"/>
       <source>Add ECC and Save As...</source>
-      <translation type="unfinished">Add ECC and Save As...</translation>
+      <translation>Adicionar ECC e Salvar Como...</translation>
     </message>
     <message>
       <location line="+6"/>
       <source>PS2 Memory Card (*.ps2 *.mc2 *.mcd);;All Files (*.*)</source>
-      <translation type="unfinished">PS2 Memory Card (*.ps2 *.mc2 *.mcd);;All Files (*.*)</translation>
+      <translation>Cartão de Memória PS2 (*.ps2 *.mc2 *.mcd);;Todos os Arquivos (*.*)</translation>
     </message>
   </context>
   <context>
@@ -167,7 +168,7 @@ For updates, visit: https://github.com/SternXD/myMCpp/releases</translation>
     <message>
       <location filename="../dialogs/SettingsDialog.cpp" line="+144"/>
       <source>Select Memory Card Folder</source>
-      <translation type="unfinished">Select Memory Card Folder</translation>
+      <translation>Escolher Pasta do Cartão de Memória</translation>
     </message>
   </context>
 </TS>
