a585b6f65efdeb3049261a988c1b5656eb9ce7b5
Stern <xsternent@gmail.com>
2025-12-21 02:14:56 -0500

    New translations mymcpp_en.ts (Romanian) [ci skip]

--- src/ui/Translations/myMCpp_ro-RO.ts
+++ src/ui/Translations/myMCpp_ro-RO.ts
@@ -2,14 +2,419 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en-US">
   <context>
+    <name>AdvancedSettingsWidget</name>
+    <message>
+      <location filename="../Settings/AdvancedSettingsWidget.cpp" line="+19"/>
+      <source>Debug Logging</source>
+      <translation type="unfinished">Debug Logging</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Enable verbose logging to standard output for debugging purposes.</source>
+      <translation type="unfinished">Enable verbose logging to standard output for debugging purposes.</translation>
+    </message>
+    <message>
+      <location line="+4"/>
+      <source>Advanced</source>
+      <translation type="unfinished">Advanced</translation>
+    </message>
+  </context>
+  <context>
+    <name>BehaviorSettingsWidget</name>
+    <message>
+      <location filename="../Settings/BehaviorSettingsWidget.cpp" line="+19"/>
+      <source>Warn Before Deleting</source>
+      <translation type="unfinished">Warn Before Deleting</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Show a warning dialog when attempting to delete files from a memory card.</source>
+      <translation type="unfinished">Show a warning dialog when attempting to delete files from a memory card.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Hide to System Tray</source>
+      <translation type="unfinished">Hide to System Tray</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Minimize the application to the system tray instead of the taskbar.</source>
+      <translation type="unfinished">Minimize the application to the system tray instead of the taskbar.</translation>
+    </message>
+    <message>
+      <location line="+5"/>
+      <source>Behavior</source>
+      <translation type="unfinished">Behavior</translation>
+    </message>
+  </context>
+  <context>
+    <name>CardActionHandler</name>
+    <message>
+      <location filename="../CardActionHandler.cpp" line="+28"/>
+      <source>Opened: %1</source>
+      <translation type="unfinished">Opened: %1</translation>
+    </message>
+    <message>
+      <location line="+7"/>
+      <location line="+22"/>
+      <location line="+24"/>
+      <location line="+61"/>
+      <location line="+36"/>
+      <location line="+36"/>
+      <location line="+37"/>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <location line="-215"/>
+      <source>Failed to open memory card: %1</source>
+      <translation type="unfinished">Failed to open memory card: %1</translation>
+    </message>
+    <message>
+      <location line="+14"/>
+      <source>Created: %1</source>
+      <translation type="unfinished">Created: %1</translation>
+    </message>
+    <message>
+      <location line="+8"/>
+      <source>Failed to create memory card: %1</source>
+      <translation type="unfinished">Failed to create memory card: %1</translation>
+    </message>
+    <message>
+      <location line="+9"/>
+      <location line="+8"/>
+      <location line="+76"/>
+      <location line="+36"/>
+      <location line="+37"/>
+      <source>No memory card open</source>
+      <translation type="unfinished">No memory card open</translation>
+    </message>
+    <message>
+      <location line="-149"/>
+      <location line="+19"/>
+      <location line="+57"/>
+      <location line="+36"/>
+      <location line="+37"/>
+      <source>Warning</source>
+      <translation type="unfinished">Warning</translation>
+    </message>
+    <message>
+      <location line="-142"/>
+      <source>File not found: %1</source>
+      <translation type="unfinished">File not found: %1</translation>
+    </message>
+    <message>
+      <location line="+13"/>
+      <source>Save file is empty or contains no valid entries</source>
+      <translation type="unfinished">Save file is empty or contains no valid entries</translation>
+    </message>
+    <message>
+      <location line="+10"/>
+      <location line="+22"/>
+      <location line="+41"/>
+      <location line="+78"/>
+      <source>Success</source>
+      <translation type="unfinished">Success</translation>
+    </message>
+    <message>
+      <location line="-140"/>
+      <source>Successfully imported save: %1</source>
+      <translation type="unfinished">Successfully imported save: %1</translation>
+    </message>
+    <message>
+      <location line="+5"/>
+      <source>Imported: %1</source>
+      <translation type="unfinished">Imported: %1</translation>
+    </message>
+    <message>
+      <location line="+5"/>
+      <source>Save Exists</source>
+      <translation type="unfinished">Save Exists</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Save &apos;%1&apos; already exists. Overwrite?</source>
+      <translation type="unfinished">Save &apos;%1&apos; already exists. Overwrite?</translation>
+    </message>
+    <message>
+      <location line="+11"/>
+      <source>Successfully imported and overwrote existing save</source>
+      <translation type="unfinished">Successfully imported and overwrote existing save</translation>
+    </message>
+    <message>
+      <location line="+4"/>
+      <source>Imported (overwrite): %1</source>
+      <translation type="unfinished">Imported (overwrite): %1</translation>
+    </message>
+    <message>
+      <location line="+11"/>
+      <source>Failed to import save: %1</source>
+      <translation type="unfinished">Failed to import save: %1</translation>
+    </message>
+    <message>
+      <location line="+26"/>
+      <source>Successfully exported save to:
+%1</source>
+      <translation type="unfinished">Successfully exported save to:
+%1</translation>
+    </message>
+    <message>
+      <location line="+4"/>
+      <source>Exported: %1</source>
+      <translation type="unfinished">Exported: %1</translation>
+    </message>
+    <message>
+      <location line="+6"/>
+      <source>Failed to export save: %1</source>
+      <translation type="unfinished">Failed to export save: %1</translation>
+    </message>
+    <message>
+      <location line="+18"/>
+      <source>Confirm Delete</source>
+      <translation type="unfinished">Confirm Delete</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Delete this save?
+
+%1</source>
+      <translation type="unfinished">Delete this save?
+
+%1</translation>
+    </message>
+    <message>
+      <location line="+11"/>
+      <source>Deleted: %1</source>
+      <translation type="unfinished">Deleted: %1</translation>
+    </message>
+    <message>
+      <location line="+6"/>
+      <source>Failed to delete save:
+
+%1</source>
+      <translation type="unfinished">Failed to delete save:
+
+%1</translation>
+    </message>
+    <message>
+      <location line="+13"/>
+      <source>Format Card</source>
+      <translation type="unfinished">Format Card</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>WARNING: This will erase ALL data on the memory card!
+
+Are you sure you want to continue?</source>
+      <translation type="unfinished">WARNING: This will erase ALL data on the memory card!
+
+Are you sure you want to continue?</translation>
+    </message>
+    <message>
+      <location line="+14"/>
+      <source>Card formatted successfully</source>
+      <translation type="unfinished">Card formatted successfully</translation>
+    </message>
+    <message>
+      <location line="+4"/>
+      <source>Memory card formatted successfully (%1 MB)</source>
+      <translation type="unfinished">Memory card formatted successfully (%1 MB)</translation>
+    </message>
+    <message>
+      <location line="+5"/>
+      <source>Failed to format card:
+
+%1</source>
+      <translation type="unfinished">Failed to format card:
+
+%1</translation>
+    </message>
+  </context>
+  <context>
+    <name>FilesSettingsWidget</name>
+    <message>
+      <location filename="../Settings/FilesSettingsWidget.cpp" line="+23"/>
+      <source>Memory Card Directory</source>
+      <translation type="unfinished">Memory Card Directory</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>The default directory where memory card images are stored.</source>
+      <translation type="unfinished">The default directory where memory card images are stored.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Browse Directory</source>
+      <translation type="unfinished">Browse Directory</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Open a file dialog to select the memory card directory.</source>
+      <translation type="unfinished">Open a file dialog to select the memory card directory.</translation>
+    </message>
+    <message>
+      <location line="+5"/>
+      <source>Files</source>
+      <translation type="unfinished">Files</translation>
+    </message>
+    <message>
+      <location line="+29"/>
+      <source>Select Memory Card Folder</source>
+      <translation type="unfinished">Select Memory Card Folder</translation>
+    </message>
+  </context>
+  <context>
+    <name>InterfaceSettingsWidget</name>
+    <message>
+      <location filename="../Settings/InterfaceSettingsWidget.cpp" line="+25"/>
+      <source>Language</source>
+      <translation type="unfinished">Language</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Select the language for the application interface.</source>
+      <translation type="unfinished">Select the language for the application interface.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Theme</source>
+      <translation type="unfinished">Theme</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Select the color theme for the application.</source>
+      <translation type="unfinished">Select the color theme for the application.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Thumbnail Size</source>
+      <translation type="unfinished">Thumbnail Size</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Adjust the size of the save icons in the main view.</source>
+      <translation type="unfinished">Adjust the size of the save icons in the main view.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Renderer</source>
+      <translation type="unfinished">Renderer</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Select the graphics API used for rendering the 3D icons.</source>
+      <translation type="unfinished">Select the graphics API used for rendering the 3D icons.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Camera Angle</source>
+      <translation type="unfinished">Camera Angle</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Change the camera angle used to view the 3D icons.</source>
+      <translation type="unfinished">Change the camera angle used to view the 3D icons.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Lighting Mode</source>
+      <translation type="unfinished">Lighting Mode</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Select how the icons are lit.</source>
+      <translation type="unfinished">Select how the icons are lit.</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Animate Icons</source>
+      <translation type="unfinished">Animate Icons</translation>
+    </message>
+    <message>
+      <location line="+0"/>
+      <source>Enable rotating animations for the 3D icons.</source>
+      <translation type="unfinished">Enable rotating animations for the 3D icons.</translation>
+    </message>
+    <message>
+      <location line="+6"/>
+      <source>None</source>
+      <translation type="unfinished">None</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Dark (Default)</source>
+      <translation type="unfinished">Dark (Default)</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Light</source>
+      <translation type="unfinished">Light</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>Windows Vista</source>
+      <translation type="unfinished">Windows Vista</translation>
+    </message>
+    <message>
+      <location line="+16"/>
+      <source>Default</source>
+      <translation type="unfinished">Default</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Flat</source>
+      <translation type="unfinished">Flat</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Near</source>
+      <translation type="unfinished">Near</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>High</source>
+      <translation type="unfinished">High</translation>
+    </message>
+    <message>
+      <location line="+3"/>
+      <source>Icon Lighting</source>
+      <translation type="unfinished">Icon Lighting</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Lighting Off</source>
+      <translation type="unfinished">Lighting Off</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Alternate 1</source>
+      <translation type="unfinished">Alternate 1</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Alternate 2</source>
+      <translation type="unfinished">Alternate 2</translation>
+    </message>
+    <message>
+      <location line="+10"/>
+      <source>Interface</source>
+      <translation type="unfinished">Interface</translation>
+    </message>
+  </context>
+  <context>
     <name>MainWindow</name>
     <message>
-      <location filename="../MainWindow.cpp" line="+81"/>
+      <location filename="../MainWindow.cpp" line="+63"/>
+      <source>Settings...</source>
+      <translation type="unfinished">Settings...</translation>
+    </message>
+    <message>
+      <location line="+22"/>
       <source>Imported %1</source>
       <translation type="unfinished">Imported %1</translation>
     </message>
     <message>
-      <location line="+27"/>
+      <location line="+31"/>
       <source>Open Memory Card</source>
       <translation type="unfinished">Open Memory Card</translation>
     </message>
@@ -25,12 +430,12 @@
     </message>
     <message>
       <location line="+2"/>
-      <location line="+274"/>
+      <location line="+275"/>
       <source>PCSX2 Memory Card (*.ps2);;MemCard PRO2 (*.mc2 *.mcd);;All Files (*.*)</source>
       <translation type="unfinished">PCSX2 Memory Card (*.ps2);;MemCard PRO2 (*.mc2 *.mcd);;All Files (*.*)</translation>
     </message>
     <message>
-      <location line="-249"/>
+      <location line="-250"/>
       <source>Created %1 MB memory card</source>
       <translation type="unfinished">Created %1 MB memory card</translation>
     </message>
@@ -90,12 +495,7 @@
       <translation type="unfinished">Deleted %1</translation>
     </message>
     <message>
-      <location line="+36"/>
-      <source>About myMCpp</source>
-      <translation type="unfinished">About myMCpp</translation>
-    </message>
-    <message>
-      <location line="+34"/>
+      <location line="+71"/>
       <source>Check for Updates</source>
       <translation type="unfinished">Check for Updates</translation>
     </message>
@@ -163,11 +563,86 @@ For updates, visit: https://github.com/SternXD/myMCpp/releases</translation>
     </message>
   </context>
   <context>
-    <name>SettingsDialog</name>
+    <name>MemoryCardBrowser</name>
     <message>
-      <location filename="../dialogs/SettingsDialog.cpp" line="+144"/>
-      <source>Select Memory Card Folder</source>
-      <translation type="unfinished">Select Memory Card Folder</translation>
+      <location filename="../widgets/MemoryCardBrowser.cpp" line="+93"/>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <location line="+1"/>
+      <source>Failed to read memory card: %1</source>
+      <translation type="unfinished">Failed to read memory card: %1</translation>
+    </message>
+  </context>
+  <context>
+    <name>SaveDetailsPanel</name>
+    <message>
+      <location filename="../widgets/SaveDetailsPanel.cpp" line="+92"/>
+      <source>Size: %1
+Modified: %2</source>
+      <translation type="unfinished">Size: %1
+Modified: %2</translation>
+    </message>
+    <message>
+      <location line="+17"/>
+      <source>
+Files: %1</source>
+      <translation type="unfinished">
+Files: %1</translation>
+    </message>
+    <message>
+      <location line="+73"/>
+      <source>No save selected</source>
+      <translation type="unfinished">No save selected</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>No details available</source>
+      <translation type="unfinished">No details available</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsWindow</name>
+    <message>
+      <location filename="../Settings/SettingsWindow.cpp" line="+34"/>
+      <source>Interface</source>
+      <translation type="unfinished">Interface</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>&lt;strong&gt;Interface Settings&lt;/strong&gt;&lt;hr&gt;These options control how the software looks throughout the application.</source>
+      <translation type="unfinished">&lt;strong&gt;Interface Settings&lt;/strong&gt;&lt;hr&gt;These options control how the software looks throughout the application.</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>Behavior</source>
+      <translation type="unfinished">Behavior</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>&lt;strong&gt;Behavior Settings&lt;/strong&gt;&lt;hr&gt;Configure how the application behaves, including warnings and shutdown confirmations.</source>
+      <translation type="unfinished">&lt;strong&gt;Behavior Settings&lt;/strong&gt;&lt;hr&gt;Configure how the application behaves, including warnings and shutdown confirmations.</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>Files</source>
+      <translation type="unfinished">Files</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>&lt;strong&gt;File Settings&lt;/strong&gt;&lt;hr&gt;Manage default paths for memory cards and other resources.</source>
+      <translation type="unfinished">&lt;strong&gt;File Settings&lt;/strong&gt;&lt;hr&gt;Manage default paths for memory cards and other resources.</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>Advanced</source>
+      <translation type="unfinished">Advanced</translation>
+    </message>
+    <message>
+      <location line="+2"/>
+      <source>&lt;strong&gt;Advanced Settings&lt;/strong&gt;&lt;hr&gt;Advanced options for debugging and developer tools.</source>
+      <translation type="unfinished">&lt;strong&gt;Advanced Settings&lt;/strong&gt;&lt;hr&gt;Advanced options for debugging and developer tools.</translation>
     </message>
   </context>
 </TS>
