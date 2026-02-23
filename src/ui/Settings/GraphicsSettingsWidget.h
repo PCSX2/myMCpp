cfb377c9c05d753b8de8df1d65b2d64dbc37e1eb
SternXD <stern@sidestore.io>
2026-02-23 13:09:33 -0500

    Qt: add General and Graphics settings widgets

---
+++ src/ui/Settings/GraphicsSettingsWidget.h
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2025-2026 SternXD
+// SPDX-License-Identifier: GPL-3.0+
+
+#pragma once
+
+#include "SettingsWidget.h"
+#include <memory>
+
+class QCheckBox;
+class QComboBox;
+class QSpinBox;
+class SettingsWindow;
+namespace Ui
+{
+	class GraphicsSettingsWidget;
+}
+
+class GraphicsSettingsWidget : public SettingsWidget
+{
+	Q_OBJECT
+
+public:
+	explicit GraphicsSettingsWidget(SettingsWindow* dialog, QWidget* parent = nullptr);
+	~GraphicsSettingsWidget();
+
+	void saveSettings() override;
+	void loadSettings() override;
+	void restoreDefaults() override;
+
+private slots:
+	void onRendererChanged(int index);
+	void onVSyncChanged(bool enabled);
+
+private:
+	std::unique_ptr<Ui::GraphicsSettingsWidget> ui;
+};
