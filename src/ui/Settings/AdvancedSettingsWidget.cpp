8510b581ab2859b7042e66bec784a9c9e76b387b
SternXD <stern@sidestore.io>
2026-02-23 13:09:58 -0500

    Qt: integrate new settings widgets and update SettingsWindow

--- src/ui/Settings/AdvancedSettingsWidget.cpp
+++ src/ui/Settings/AdvancedSettingsWidget.cpp
@@ -5,6 +5,8 @@
 #include "SettingsWindow.h"
 #include "ui_AdvancedSettingsWidget.h"
 #include "Config.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
 AdvancedSettingsWidget::AdvancedSettingsWidget(SettingsWindow* dialog, QWidget* parent)
 	: SettingsWidget(dialog, parent)
@@ -14,8 +16,12 @@ AdvancedSettingsWidget::AdvancedSettingsWidget(SettingsWindow* dialog, QWidget* 
 	ui->setupUi(container);
 
 	registerHelp(ui->enableDebugLogCheck, tr("Debug Logging"), tr("Enable verbose logging to standard output for debugging purposes."));
+	registerHelp(ui->exportSettingsButton, tr("Export Settings"), tr("Save the current settings to a JSON file for backup or sharing."));
+	registerHelp(ui->importSettingsButton, tr("Import Settings"), tr("Load settings from a previously exported JSON file."));
 
 	connect(ui->enableDebugLogCheck, &QCheckBox::toggled, this, &SettingsWidget::settingChanged);
+	connect(ui->exportSettingsButton, &QPushButton::clicked, this, &AdvancedSettingsWidget::onExportSettings);
+	connect(ui->importSettingsButton, &QPushButton::clicked, this, &AdvancedSettingsWidget::onImportSettings);
 
 	addTab(tr("Advanced"), container);
 	loadSettings();
@@ -26,7 +32,8 @@ AdvancedSettingsWidget::~AdvancedSettingsWidget() = default;
 void AdvancedSettingsWidget::loadSettings()
 {
 	Config* config = m_dialog->getConfig();
-	if (!config) return;
+	if (!config)
+		return;
 
 	ui->enableDebugLogCheck->setChecked(config->getDebugLogging());
 }
@@ -34,7 +41,8 @@ void AdvancedSettingsWidget::loadSettings()
 void AdvancedSettingsWidget::saveSettings()
 {
 	Config* config = m_dialog->getConfig();
-	if (!config) return;
+	if (!config)
+		return;
 
 	config->setDebugLogging(ui->enableDebugLogCheck->isChecked());
 }
@@ -42,4 +50,65 @@ void AdvancedSettingsWidget::saveSettings()
 void AdvancedSettingsWidget::restoreDefaults()
 {
 	ui->enableDebugLogCheck->setChecked(false);
+}
+
+void AdvancedSettingsWidget::onExportSettings()
+{
+	Config* config = m_dialog->getConfig();
+	if (!config)
+		return;
+
+	QString fileName = QFileDialog::getSaveFileName(this,
+		tr("Export Settings"),
+		"myMCpp-settings.json",
+		tr("JSON Files (*.json)"));
+
+	if (!fileName.isEmpty())
+	{
+		if (config->saveAs(fileName.toStdString()))
+		{
+			QMessageBox::information(this, tr("Export Successful"),
+				tr("Settings have been exported to %1").arg(fileName));
+		}
+		else
+		{
+			QMessageBox::warning(this, tr("Export Failed"),
+				tr("Failed to export settings to %1").arg(fileName));
+		}
+	}
+}
+
+void AdvancedSettingsWidget::onImportSettings()
+{
+	Config* config = m_dialog->getConfig();
+	if (!config)
+		return;
+
+	QString fileName = QFileDialog::getOpenFileName(this,
+		tr("Import Settings"),
+		QString(),
+		tr("JSON Files (*.json)"));
+
+	if (!fileName.isEmpty())
+	{
+		QMessageBox::StandardButton reply = QMessageBox::question(this, tr("Confirm Import"),
+			tr("This will overwrite your current settings. Are you sure you want to continue?"),
+			QMessageBox::Yes | QMessageBox::No);
+
+		if (reply == QMessageBox::Yes)
+		{
+			if (config->load(fileName.toStdString()))
+			{
+				loadSettings();
+				emit settingChanged();
+				QMessageBox::information(this, tr("Import Successful"),
+					tr("Settings have been imported from %1").arg(fileName));
+			}
+			else
+			{
+				QMessageBox::warning(this, tr("Import Failed"),
+					tr("Failed to import settings from %1").arg(fileName));
+			}
+		}
+	}
 }
--- src/ui/Settings/FilesSettingsWidget.cpp
+++ src/ui/Settings/FilesSettingsWidget.cpp
@@ -6,6 +6,8 @@
 #include "ui_FilesSettingsWidget.h"
 #include "Config.h"
 #include <QFileDialog>
+#include <QListWidget>
+#include <QMessageBox>
 
 FilesSettingsWidget::FilesSettingsWidget(SettingsWindow* dialog, QWidget* parent)
 	: SettingsWidget(dialog, parent)
@@ -16,9 +18,18 @@ FilesSettingsWidget::FilesSettingsWidget(SettingsWindow* dialog, QWidget* parent
 
 	registerHelp(ui->memoryCardPathEdit, tr("Memory Card Directory"), tr("The default directory where memory card images are stored."));
 	registerHelp(ui->browseButton, tr("Browse Directory"), tr("Open a file dialog to select the memory card directory."));
+	registerHelp(ui->importExportPathEdit, tr("Import/Export Directory"), tr("The default directory for importing and exporting save files. If not set, the memory card directory is used."));
+	registerHelp(ui->browseImportExportButton, tr("Browse Directory"), tr("Open a file dialog to select the import/export directory."));
+	registerHelp(ui->recentFilesList, tr("Recent Files"), tr("List of recently opened memory card files."));
+	registerHelp(ui->clearRecentButton, tr("Clear Recent Files"), tr("Remove all entries from the recent files list."));
 
 	connect(ui->browseButton, &QPushButton::clicked, this, &FilesSettingsWidget::onBrowseMemoryCardPath);
+	connect(ui->browseImportExportButton, &QPushButton::clicked, this, &FilesSettingsWidget::onBrowseImportExportPath);
+	connect(ui->clearRecentButton, &QPushButton::clicked, this, &FilesSettingsWidget::onClearRecentFiles);
 	connect(ui->memoryCardPathEdit, &QLineEdit::textChanged, this, &SettingsWidget::settingChanged);
+	connect(ui->importExportPathEdit, &QLineEdit::textChanged, this, &SettingsWidget::settingChanged);
+
+	ui->recentFilesList->setSelectionMode(QAbstractItemView::NoSelection);
 
 	addTab(tr("Files"), container);
 	loadSettings();
@@ -29,21 +40,78 @@ FilesSettingsWidget::~FilesSettingsWidget() = default;
 void FilesSettingsWidget::loadSettings()
 {
 	Config* config = m_dialog->getConfig();
-	if (!config) return;
+	if (!config)
+		return;
 
 	ui->memoryCardPathEdit->setText(QString::fromStdString(config->getMemoryCardFolder()));
+	ui->importExportPathEdit->setText(QString::fromStdString(config->getImportExportFolder()));
+
+	// Load recent files from config
+	ui->recentFilesList->clear();
+	const auto& recentFiles = config->getRecentFiles();
+	for (const auto& file : recentFiles)
+	{
+		ui->recentFilesList->addItem(QString::fromStdString(file));
+	}
+
+	if (ui->recentFilesList->count() == 0)
+	{
+		ui->recentFilesList->addItem(tr("No recent files"));
+		ui->recentFilesList->setEnabled(false);
+		ui->clearRecentButton->setEnabled(false);
+	}
+	else
+	{
+		ui->recentFilesList->setEnabled(true);
+		ui->clearRecentButton->setEnabled(true);
+	}
 }
 
 void FilesSettingsWidget::saveSettings()
 {
 	Config* config = m_dialog->getConfig();
-	if (!config) return;
+	if (!config)
+		return;
 
 	config->setMemoryCardFolder(ui->memoryCardPathEdit->text().toStdString());
+	config->setImportExportFolder(ui->importExportPathEdit->text().toStdString());
 }
 
 void FilesSettingsWidget::restoreDefaults()
 {
+	ui->memoryCardPathEdit->clear();
+	ui->importExportPathEdit->clear();
+}
+
+void FilesSettingsWidget::onClearRecentFiles()
+{
+	Config* config = m_dialog->getConfig();
+	if (!config)
+		return;
+
+	QMessageBox::StandardButton reply = QMessageBox::question(this, tr("Clear Recent Files"),
+		tr("Are you sure you want to clear all recent files?"),
+		QMessageBox::Yes | QMessageBox::No);
+
+	if (reply == QMessageBox::Yes)
+	{
+		config->clearRecentFiles();
+		ui->recentFilesList->clear();
+		ui->recentFilesList->addItem(tr("No recent files"));
+		ui->recentFilesList->setEnabled(false);
+		ui->clearRecentButton->setEnabled(false);
+		emit settingChanged();
+	}
+}
+
+void FilesSettingsWidget::onBrowseImportExportPath()
+{
+	QString dir = QFileDialog::getExistingDirectory(this,
+		tr("Select Import/Export Folder"), ui->importExportPathEdit->text());
+	if (!dir.isEmpty())
+	{
+		ui->importExportPathEdit->setText(dir);
+	}
 }
 
 void FilesSettingsWidget::onBrowseMemoryCardPath()
