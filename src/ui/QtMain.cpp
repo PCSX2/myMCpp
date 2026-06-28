8bd6d29b2ace4ba386a69aa17d8ad08453d525fc
SternXD <stern@sidestore.io>
2026-06-28 14:49:21 -0400

    Qt: Fix resources path on Linux AppImage

--- src/ui/QtMain.cpp
+++ src/ui/QtMain.cpp
@@ -6,14 +6,37 @@
 #include "Config.h"
 #include "MainWindow.h"
 #include "TranslationManager.h"
+#include "ResourcePath.h"
 #include "version.h"
 #include "Logger.h"
 
+#include <QCoreApplication>
 #include <QLibraryInfo>
+
+#if defined(__APPLE__)
+#include "CocoaTools.h"
+#endif
+
+namespace fs = std::filesystem;
+
+static void initResourcePath(Config& config)
+{
+#if defined(__APPLE__)
+	if (auto bundlePath = CocoaTools::GetResourcePath())
+		config.setResourcesPath(*bundlePath);
+	else
+		config.setResourcesPath("resources");
+#else
+	config.setResourcesPath(fs::path(QCoreApplication::applicationDirPath().toStdString()) / "resources");
+#endif
+	ResourcePath::set(config.getResourcesPath());
+	Logger::info("Main: Resources path: {}", ResourcePath::get().string());
+}
 
 int runQtMainApp(int argc, char* argv[], Config& config)
 {
 	QApplication app(argc, argv);
+	initResourcePath(config);
 	app.setApplicationName(MYMCpp_APP_NAME);
 	app.setApplicationVersion(myMCpp_VERSION_STRING);
 	app.setOrganizationName("myMCpp");
